6f95d4fea0fda9bc254dc2dbcc5698cd6885692c
Dmytro Turskyi <dmytro@turskyi.com>
2024-02-25 17:31:49 -0500

    Make home widget more fancy.
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- android/app/src/main/java/com/turskyi/news_glance/NewsWidget.kt
+++ android/app/src/main/java/com/turskyi/news_glance/NewsWidget.kt
@@ -3,6 +3,7 @@ package com.turskyi.news_glance
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.SharedPreferences
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetPlugin
 
@@ -36,13 +37,13 @@ internal fun updateAppWidget(
     appWidgetId: Int
 ) {
     // Get reference to SharedPreferences
-    val widgetData = HomeWidgetPlugin.getData(context)
-    val views = RemoteViews(context.packageName, R.layout.news_widget).apply {
+    val widgetData: SharedPreferences = HomeWidgetPlugin.getData(context)
+    val views: RemoteViews = RemoteViews(context.packageName, R.layout.news_widget).apply {
 
-        val title = widgetData.getString("headline_title", null)
+        val title: String? = widgetData.getString("headline_title", null)
         setTextViewText(R.id.headline_title, title ?: "No title set")
 
-        val description = widgetData.getString("headline_description", null)
+        val description: String? = widgetData.getString("headline_description", null)
         setTextViewText(R.id.headline_description, description ?: "No description set")
     }
 
