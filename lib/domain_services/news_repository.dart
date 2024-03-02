ff4e5b3e344c85304c3af517d5176498ab2e4338
Dmytro Turskyi <dmytro@turskyi.com>
2024-03-02 14:47:52 -0500

    Update README.md, by adding an architecture pattern.
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/domain_services/news_repository.dart
+++ lib/domain_services/news_repository.dart
@@ -1,7 +1,7 @@
 import 'package:news_glance/domain_models/news_article.dart';
 import 'package:news_glance/res/constants.dart' as country;
 
-abstract class NewsRepository {
+abstract interface class NewsRepository {
   const NewsRepository();
 
   Future<List<NewsArticle>> getNews({String countryCode = country.canadaCode});
