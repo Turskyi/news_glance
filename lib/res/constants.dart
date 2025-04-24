a37cf3fd8890b1d5fb56464c23c6d19d750c1142
Dmytro Turskyi <dmytro@turskyi.com>
2025-04-23 21:01:36 -0400

    :arrow_up: Refactor address format in constants.
    * Update the address format in `constants.dart` to include "Harmony Village" and use a more concise layout.
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -2,14 +2,13 @@ const String iOSWidgetName = 'NewsWidgets';
 const String androidWidgetName = 'NewsWidget';
 const String appGroupId = 'group.dmytrowidget';
 const String baseUrl = 'https://news.turskyi.com/api/';
-const String canadaCode = 'CA';
 const String usaCode = 'US';
 const String website = 'https://news.turskyi.com';
 const String email = 'dmytro@turskyi.com';
 const String phone = '+14379852581';
 const String address =
-    'Address:\n3035 Finch West Avenue.,\nNorth York\nOntario\nM9M 0A3\n'
-    'Canada.';
+    'Address:\nHarmony Village\n3035 Finch West Avenue.,\nNorth York\nOntario\n'
+    'M9M 0A3\nCanada.';
 const int newsMax = 10;
 const double defaultExpandedHeight = 278.0;
 const String appName = 'News Glance';
