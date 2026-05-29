7bf92158018b817483fb6142e757988991c62319
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2026-05-29 17:03:41 -0400

    :wrench: Increase news limit and refactor localization imports
    *   **Update Constants**: Increase `newsMax` from 10 to 12 in `constants.dart`.
    *   **Refactor Localizations**: Remove unnecessary empty lines in `app_localizations_uk.dart` and `app_localizations_en.dart`.
    
    Signed-off-by: Dmytro Turskyi <dmytro.turskyi@gmail.com>

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -14,7 +14,7 @@ const String phone = '+14379852581';
 const String address =
     'Address:\nHarmony Village\n3035 Finch West Avenue.,\nNorth York\nOntario\n'
     'M9M 0A3\nCanada.';
-const int newsMax = 10;
+const int newsMax = 12;
 const double defaultExpandedHeight = 278.0;
 const String appName = 'News Glance';
 
